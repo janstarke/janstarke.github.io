0d6728ca4e51298a0423b220da7727a36647f738
Jan Starke <jan.starke@telekom.de>
2023-08-19 12:16:56 +0200

    add repo name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'janstarke', // Usually your GitHub org/user name.
-  projectName: 'janstarke', // Usually your repo name.
+  projectName: 'janstarke.github.io', // Usually your repo name.
 	deploymentBranch: 'gh-pages',
 	trailingSlash: false,
 
